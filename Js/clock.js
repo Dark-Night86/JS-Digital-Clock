f12078db94852faded883cb2a894eac5c422687b
Ali <98445430+Dark-Night86@users.noreply.github.com>
2022-07-14 21:42:53 +0430

    Release 2.0 Upload
    Whole JS mechanics changed , and now the clock is more bigger than Release 1.0 .

--- Js/clock.js
+++ Js/clock.js
@@ -1,5 +1,3 @@
-'use strict'
-
 
 //! variables =>
 
@@ -7,33 +5,13 @@ var h = document.querySelector('#hours');
 var m = document.querySelector('#minutes');
 var s = document.querySelector('#seconds');
 
-//! adding the real time to html file =>
-
-var time = new Date();
-var s_number = time.getSeconds();
-var m_number = time.getMinutes();
-var h_number = time.getHours();
-
 //! main counting operation =>
 
 setInterval(() => {
 
-    s.innerHTML = s_number;
-    m.innerHTML = m_number;
-    h.innerHTML = h_number;
-
-    s_number += 1;
-
-    if (s_number == 60) {
-        clearInterval();
-        s_number -= 60;
-        m_number = m_number + 1;
-
-        if (m_number == 60) {
-            h_number = h_number + 1;
-            m_number -= 60;
-        }
-    }
+    var time = new Date();
+    s.innerHTML = time.getSeconds();
+    m.innerHTML = time.getMinutes();
+    h.innerHTML = time.getHours();
 
 }, 1000);
-
